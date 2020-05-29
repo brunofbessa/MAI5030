dce7ece2dd777ceb7f46f716f146be7fbf4402f3
Bruno F. Bessa <brunofbessa@gmail.com>
2020-05-29 10:23:25 -0300

    Primeiro plot eaf

--- projetos_mai5030/multi-obejctive_problem/test_nsgaii.py
+++ projetos_mai5030/multi-obejctive_problem/test_nsgaii.py
@@ -1,10 +1,11 @@
 import nsgaii
 import matplotlib.pyplot as plt
 from scipy import stats
+import pandas as pd
 
 NUM_EXEC = 1
 CONST_MAX_GERACOES = 10
-CONST_TAM_POP = 100
+CONST_TAM_POP = 50
 lista_tam_populacoes = [10, 25, 50, 75, 100]
 lista_num_geracoes = [25, 50, 75, 100]
 num_genes = 1
@@ -14,17 +15,29 @@ taxa_mutacao = 0.05
 
 f1 = []
 f2 = []
+run = []
+i = 1
 for num_geracoes in lista_num_geracoes:
 	args = [num_genes, CONST_TAM_POP, num_geracoes, taxa_crossover, taxa_mutacao]
 	_f1, _f2 = nsgaii.main(args)
-	print(num_geracoes, len(_f1), len(_f2))
+	for _i in _f1:
+		run.append(i)
 	f1 += _f1
 	f2 += _f2
+	i += 1
 
-print(len(f1), len(f2))
+df = pd.DataFrame()
+df['f1'] = f1
+df['f2'] = f2
+df['run'] = run
+
+df.to_csv('nsgaii.csv', index=False)
+
+#print(df)
+
 
 plt.xlabel('Fun. -(x)^2')
 plt.ylabel('Fun. -(x-2)ˆ2')
 plt.scatter(f1, f2)
 print()
-plt.show()
+#plt.show()
